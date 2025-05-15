2f8d2761afa4acedb8adfa9e218c6bfadc213fbf
Taiga Ishida <tonythetaiga@gmail.com>
2025-05-15 17:47:20 -0400

    turn on autocomplete for the login

--- web/src/routes/auth/+page.svelte
+++ web/src/routes/auth/+page.svelte
@@ -3,18 +3,20 @@
     class="text-ctp-text max-w-md p-8 rounded bg-ctp-base flex flex-col space-y-2"
     method="POST"
     action="?/signup"
+    autocomplete="on"
+    name="login-form"
   >
     <label>
       Email
-      <input name="email" type="email" class="bg-ctp-mantle px-2" />
+      <input name="email" type="email" class="bg-ctp-mantle px-2" autocomplete="username email" />
     </label>
     <label>
       Password
-      <input name="password" type="password" class="bg-ctp-mantle px-2" />
+      <input name="password" type="password" class="bg-ctp-mantle px-2" autocomplete="current-password" />
     </label>
     <div>
-      <button>Sign up</button>
-      <button formaction="?/signin">Sign In</button>
+      <button type="submit">Sign up</button>
+      <button type="submit" formaction="?/signin">Sign In</button>
     </div>
   </form>
 </div>
