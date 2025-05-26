8ee411d5ce867ffecb5ad1b2c471b19906b7c3b4
Taiga Ishida <tonythetaiga@gmail.com>
2025-05-26 10:33:44 -0400

    size tora with clamp rather than cqw

--- web/src/lib/components/landing-page.svelte
+++ web/src/lib/components/landing-page.svelte
@@ -4,18 +4,18 @@
 
 <section class="flex flex-col h-full p-8 mx-auto text-ctp-text">
   <div class="grid grid-cols-3 grid-rows-2 w-full flex-auto">
-    <!-- 1) Top-left: Tora spans cols 1 & 2 = 2/3 -->
+    <!-- 1) Top-left: (2/3) -->
     <div class="col-span-2 flex items-end justify-start text-start">
-      <h1 class="font-extrabold leading-none text-[18.5cqw]">TORA</h1>
+      <h1 class="font-extrabold leading-none text-[clamp(8rem,18vw,32rem)]">TORA</h1>
     </div>
 
     <!-- 2) Top-right: empty cell (1/3) -->
     <div></div>
 
-    <!-- 3) Bottom-left: empty cell (1/3) -->
+    <!-- 3) Bottom-left: empty cell (2/3) -->
     <div class="col-span-2"></div>
 
-    <!-- 4) Bottom-right: copy spans cols 2 & 3 = 2/3 -->
+    <!-- 4) Bottom-right: (1/3) -->
     <div class="flex flex-col">
       <p class="text-base leading-relaxed">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dolor sit amet
@@ -24,7 +24,7 @@
         elit quisque faucibus.
       </p>
       <button class="mt-4 text-start">
-        <span class="p-2 rounded-lg bg-ctp-blue/20 hover:bg-ctp-blue"
+        <span class="p-2 rounded-lg bg-ctp-blue/20 hover:bg-ctp-blue transition-colors"
           >Create Experiment</span
         >
       </button>
