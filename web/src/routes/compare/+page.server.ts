43034600f116be4aa852acb4756d9fda8396874c
Taiga Ishida <tonythetaiga@gmail.com>
2025-06-04 14:43:53 -0400

    use new rpc to get experiments for comparisons

--- web/src/routes/compare/+page.server.ts
+++ web/src/routes/compare/+page.server.ts
@@ -1,16 +1,21 @@
 import { error } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 
-export const load: PageServerLoad = async ({ url }) => {
+export const load: PageServerLoad = async ({ url, locals }) => {
   const idsParam = url.searchParams.get("ids");
-
   if (!idsParam) {
     throw error(400, "Missing 'ids' query parameter");
   }
+  const ids = idsParam.split(",").filter(Boolean);
+  const { data: experiments, error: supError } = await locals.supabase.rpc(
+    "get_experiments_with_metrics_names",
+    { experiment_ids: ids },
+  );
 
-  const ids = idsParam.split(",").filter(Boolean); // Filter handles accidental trailing commas
-  console.log(ids);
-  return {
-    ids,
-  };
+  if (supError) {
+    console.log(supError);
+    throw error(400, "Failed to get experiments");
+  }
+
+  return { experiments };
 };
