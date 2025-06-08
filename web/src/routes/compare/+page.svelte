b8aaa7f67674d9215f7d66b537ebcb06fca1e70b
Taiga Ishida <tonythetaiga@gmail.com>
2025-06-08 11:41:59 -0400

    wip radar chart

--- web/src/routes/compare/+page.svelte
+++ web/src/routes/compare/+page.svelte
@@ -94,26 +94,51 @@
       return;
     }
 
-    const data = [
-      { year: 2010, count: 10 },
-      { year: 2011, count: 20 },
-      { year: 2012, count: 15 },
-      { year: 2013, count: 25 },
-      { year: 2014, count: 22 },
-      { year: 2015, count: 30 },
-      { year: 2016, count: 28 },
-    ];
+    const data = {
+      labels: [
+        "Eating",
+        "Drinking",
+        "Sleeping",
+        "Designing",
+        "Coding",
+        "Cycling",
+        "Running",
+      ],
+      datasets: [
+        {
+          label: "My First Dataset",
+          data: [65, 59, 90, 81, 56, 55, 40],
+          fill: true,
+          backgroundColor: "rgba(255, 99, 132, 0.2)",
+          borderColor: "rgb(255, 99, 132)",
+          pointBackgroundColor: "rgb(255, 99, 132)",
+          pointBorderColor: "#fff",
+          pointHoverBackgroundColor: "#fff",
+          pointHoverBorderColor: "rgb(255, 99, 132)",
+        },
+        {
+          label: "My Second Dataset",
+          data: [28, 48, 40, 19, 96, 27, 100],
+          fill: true,
+          backgroundColor: "rgba(54, 162, 235, 0.2)",
+          borderColor: "rgb(54, 162, 235)",
+          pointBackgroundColor: "rgb(54, 162, 235)",
+          pointBorderColor: "#fff",
+          pointHoverBackgroundColor: "#fff",
+          pointHoverBorderColor: "rgb(54, 162, 235)",
+        },
+      ],
+    };
 
     new Chart(chart, {
-      type: "bar",
-      data: {
-        labels: data.map((row) => row.year),
-        datasets: [
-          {
-            label: "Acquisitions by year",
-            data: data.map((row) => row.count),
+      type: "radar",
+      data: data,
+      options: {
+        elements: {
+          line: {
+            borderWidth: 3,
           },
-        ],
+        },
       },
     });
   };
@@ -236,7 +261,7 @@
     </div>
   </div>
 
-  <div class="border border-ctp-surface0 bg-ctp-base">
+  <div class="border border-ctp-surface0 bg-ctp-base w-1/2">
     <div
       class="text-xs font-medium text-ctp-subtext1 p-2 bg-ctp-mantle border-b border-ctp-surface0"
     >
