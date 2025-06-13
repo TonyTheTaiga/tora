0776f162cfd185d672f29833ff8bc393557607cb
Taiga Ishida <tonythetaiga@gmail.com>
2025-06-13 19:04:15 -0400

    fix runes

--- web/src/routes/workspaces/[slug]/+page.svelte
+++ web/src/routes/workspaces/[slug]/+page.svelte
@@ -1,7 +1,39 @@
 <script lang="ts">
+  import {
+    getCreateExperimentModal,
+    getEditExperimentModal,
+    getDeleteExperimentModal,
+  } from "$lib/state/app.svelte.js";
   import ExperimentsList from "$lib/components/experiments-list.svelte";
-  let { data } = $props();
-  let { experiments, workspace } = $derived(data);
+  import CreateExperimentModal from "$lib/components/create-experiment-modal.svelte";
+  import DeleteConfirmationModal from "$lib/components/delete-confirmation-modal.svelte";
+  import EditExperimentModal from "$lib/components/edit-experiment-modal.svelte";
+
+  let { data = $bindable() } = $props();
+  let { workspace } = $derived(data);
+  let experiments = $state(data.experiments);
+
+  $effect(() => {
+    experiments = data.experiments;
+  });
+
+  let createExperimentModal = $derived(getCreateExperimentModal());
+  let editExperimentModal = $derived(getEditExperimentModal());
+  let deleteExperimentModal = $derived(getDeleteExperimentModal());
 </script>
 
-<ExperimentsList {experiments} />
+{#if createExperimentModal}
+  <CreateExperimentModal />
+{/if}
+
+{#if deleteExperimentModal}
+  <DeleteConfirmationModal
+    experiment={deleteExperimentModal}
+    bind:experiments
+  />
+{/if}
+
+{#if editExperimentModal}
+  <EditExperimentModal experiment={editExperimentModal} />
+{/if}
+<ExperimentsList bind:experiments />
