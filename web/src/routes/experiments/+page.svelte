c73a32c1ee60c6f811831f52c5f4a7bcaf7c66b2
Taiga Ishida <tonythetaiga@gmail.com>
2025-06-15 02:11:31 -0400

    Move experiments list to world

--- web/src/routes/experiments/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-  import PublicExperiments from "$lib/components/public-experiments.svelte";
-  let { data } = $props();
-</script>
-
-<PublicExperiments experiments={data.experiments} />
-
