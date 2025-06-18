2b2f08c6a57a8ce16ae012a9fb2860c776bd9c78
Taiga Ishida <tonythetaiga@gmail.com>
2025-06-17 23:58:33 -0400

    Pypi stuff (#79)
    * iterate on tora client
    
    * everything working
    
    * update wheel

--- python/tora/tora.py
+++ python/tora/tora.py
@@ -2,27 +2,31 @@ import httpx
 import os
 
 
-TORA_BASE_URL = "http://localhost:5173/api"
-TORA_DEV_KEY = "tosk_e5a828992e556642ba0d3edb097ca131a677188ef36d39dd"
-TORA_API_KEY = os.getenv("TORA_API_KEY", TORA_DEV_KEY)
+TORA_BASE_URL = "https://happyplace-1030250455947.us-central1.run.app/api"
+TORA_API_KEY = os.getenv("TORA_API_KEY", None)
 
 
 def hp_to_tora_format(
-    hp: dict[str, str | int | float],
+    hp: dict[str | int | float, str | int | float],
 ) -> list[dict[str, str | int | float]]:
     return [{"key": key, "value": value} for key, value in hp.items()]
 
 
 def hp_from_tora_format(
     hp_list: list[dict[str, str | int | float]],
-) -> dict[str, str | int | float]:
-    """Convert hyperparameters from the API format to a dictionary."""
+) -> dict[str | int | float, str | int | float]:
     return {single["key"]: single["value"] for single in hp_list}
 
 
 def create_workspace(
-    name: str, description: str = "", api_key: str = TORA_API_KEY
+    name: str, description: str = "", api_key: str | None = None
 ) -> str:
+    api_key = api_key or TORA_API_KEY
+    if api_key is None:
+        raise RuntimeError(
+            "Unable to find TORA_API_KEY in the environment. Eithr specify it in the environment or pass it as a paramter to this function."
+        )
+
     http_client = httpx.Client(
         base_url=TORA_BASE_URL,
         headers={
@@ -42,12 +46,12 @@ class Tora:
         self,
         experiment_id: str,
         workspace_id: str,
+        api_key: str,
         description: str | None = None,
-        hyperparams: dict[str, str | int | float] | None = None,
+        hyperparams: dict[str | int | float, str | int | float] | None = None,
         tags: list[str] | None = None,
         server_url: str = TORA_BASE_URL,
         max_buffer_len: int = 25,
-        api_key: str = TORA_API_KEY,
     ):
         self._experiment_id = experiment_id
         self._workspace_id = workspace_id
@@ -63,7 +67,7 @@ class Tora:
                 "Content-Type": "application/json",
             },
         )
-        
+
     @property
     def max_buffer_len(self) -> int:
         return self._max_buffer_len
@@ -78,12 +82,20 @@ class Tora:
         workspace_id: str,
         name: str,
         description: str | None = None,
-        hyperparams: dict[str, str | int | float] | None = None,
+        hyperparams: dict[str | int | float, str | int | float] | None = None,
         tags: list[str] | None = None,
-        server_url: str = TORA_BASE_URL,
         max_buffer_len: int = 25,
-        api_key: str = TORA_API_KEY,
+        api_key: str | None = None,
+        server_url: str | None = None,
     ):
+        api_key = api_key or TORA_API_KEY
+        if api_key is None:
+            raise RuntimeError(
+                "Unable to find TORA_API_KEY in the environment. Eithr specify it in the environment or pass it as a paramter to this function."
+            )
+
+        server_url = server_url or TORA_BASE_URL
+
         data = {}
         data["name"] = name
         data["workspaceId"] = workspace_id
@@ -125,10 +137,18 @@ class Tora:
     def load_experiment(
         cls,
         experiment_id: str,
-        server_url: str = "http://localhost:5173/api",
         max_buffer_len: int = 25,
-        api_key: str = TORA_API_KEY,
+        api_key: str | None = None,
+        server_url: str | None = None,
     ):
+        api_key = api_key or TORA_API_KEY
+        if api_key is None:
+            raise RuntimeError(
+                "Unable to find TORA_API_KEY in the environment. Eithr specify it in the environment or pass it as a paramter to this function."
+            )
+
+        server_url = server_url or TORA_BASE_URL
+
         req = httpx.get(
             url=server_url + f"/experiments/{experiment_id}",
             headers={"x-api-key": api_key},
@@ -142,14 +162,14 @@ class Tora:
         tags = data["tags"]
 
         return cls(
-            experiment_id,
-            workspace_id,
-            description,
-            hyperparams,
-            tags,
-            server_url,
-            max_buffer_len=max_buffer_len,
+            experiment_id=experiment_id,
+            workspace_id=workspace_id,
             api_key=api_key,
+            description=description,
+            hyperparams=hyperparams,
+            tags=tags,
+            server_url=server_url,
+            max_buffer_len=max_buffer_len,
         )
 
     def log(self, name, value, step: int | None = None, metadata: dict | None = None):
