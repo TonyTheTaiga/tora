693123e27cfe159e2c508f34d5db6e69d67527b5
Taiga Ishida <tonythetaiga@gmail.com>
2025-06-26 10:33:30 -0400

    break out header into its own component for reusability

---
+++ web/src/lib/components/header.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { goto } from "$app/navigation";
+  import Logo from "./logo_assets/logo.svelte";
+</script>
+
+<header class="sticky top-0 z-30">
+  <nav
+    class="px-4 sm:px-6 py-3 sm:py-4 flex flex-row justify-between items-center bg-ctp-mantle border-b border-ctp-surface0"
+  >
+    <!-- Logo -->
+    <button class="w-32 text-ctp-blue fill-current" onclick={() => goto("/")}>
+      <Logo />
+    </button>
+  </nav>
+</header>
--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -2,7 +2,7 @@
   import "../app.css";
   import { invalidate } from "$app/navigation";
   import { onMount } from "svelte";
-  import Logo from "$lib/components/logo_assets/logo.svelte";
+  import Header from "$lib/components/header.svelte";
   import { goto } from "$app/navigation";
   import Toolbar from "$lib/components/toolbar.svelte";
 
@@ -21,16 +21,7 @@
 </script>
 
 {#if user}
-  <header class="sticky top-0 z-30">
-    <nav
-      class="px-4 sm:px-6 py-3 sm:py-4 flex flex-row justify-between items-center bg-ctp-mantle border-b border-ctp-surface0"
-    >
-      <!-- Logo -->
-      <button class="w-32 text-ctp-blue fill-current" onclick={() => goto("/")}>
-        <Logo />
-      </button>
-    </nav>
-  </header>
+  <Header />
   <Toolbar />
 {/if}
 
