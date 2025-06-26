69255f63a591686ccdd60bf89aad296a7f438b5b
Taiga Ishida <tonythetaiga@gmail.com>
2025-06-26 11:49:10 -0400

    add docs page

---
+++ web/src/routes/docs/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import Header from "$lib/components/header.svelte";
+</script>
+
+<Header />
+
+<div class="p-4 md:p-6">
+  <div class="flex items-stretch gap-3 md:gap-4 min-w-0 flex-1 pr-4 min-h-fit mb-6">
+    <div class="w-2 bg-ctp-blue rounded-full flex-shrink-0 self-stretch"></div>
+    <div class="min-w-0 flex-1 py-1">
+      <h1 class="text-lg md:text-xl text-ctp-text truncate font-mono">
+        documentation
+      </h1>
+      <div class="text-sm text-ctp-subtext0 space-y-1">
+        <div>working hard, brb w some docs</div>
+      </div>
+    </div>
+  </div>
+</div>
