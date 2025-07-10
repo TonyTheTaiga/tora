c44d35df9a8a03eb11abd0ab60bbaa2bc7eb214d
Taiga Ishida <tonythetaiga@gmail.com>
2025-07-10 00:48:21 -0400

    add error page

---
+++ web/src/routes/+error.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { page } from "$app/state";
+</script>
+
+<div class="font-mono text-ctp-text p-4">
+  <div class="space-y-4">
+    <div>
+      {#if page && page.error}
+        {page.error.message}
+      {:else}
+        something went horribly wrong
+      {/if}
+    </div>
+
+    <a
+      href="/"
+      class="text-ctp-blue hover:text-ctp-blue/80 transition-colors text-sm"
+    >
+      [back to workspace]
+    </a>
+  </div>
+</div>
