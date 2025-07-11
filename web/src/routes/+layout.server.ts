7603d0cf00b06c6729785eb6e24f1e10aee533c4
Taiga Ishida <tonythetaiga@gmail.com>
2025-07-11 01:01:30 -0400

    more redirect fun

--- web/src/routes/+layout.server.ts
+++ web/src/routes/+layout.server.ts
@@ -9,5 +9,12 @@ export const load: LayoutServerLoad = async ({ locals, url }) => {
     url.pathname !== "/"
   ) {
     return redirect(302, "/login");
+  } else if (
+    locals.session &&
+    (url.pathname === "/login" ||
+      url.pathname.startsWith("/signup") ||
+      url.pathname === "/")
+  ) {
+    return redirect(302, "/workspaces");
   }
 };
