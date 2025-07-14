cb35908aa14e368e527e4160180a5dc3b6c10b71
Taiga Ishida <tonythetaiga@gmail.com>
2025-07-14 11:54:48 -0400

    deploy frontend to vercel

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,12 +1,16 @@
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
-import adapter from "@sveltejs/adapter-node";
+// import adapter from "@sveltejs/adapter-node";
+import adapter from "@sveltejs/adapter-vercel";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
 
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      // runtime: "nodejs22.x",
+      runtime: "edge",
+    }),
   },
   csrf: {
     checkOrigin: process.env.NODE_ENV === "production" ? true : false,
