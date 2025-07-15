961edbfa6fcdecab0c295989c6713f79ba5ff3a4
Taiga Ishida <tonythetaiga@gmail.com>
2025-07-14 23:50:42 -0400

    state

--- api/src/state.rs
+++ api/src/state.rs
@@ -1,7 +1,8 @@
 use crate::settings::Settings;
+use sqlx::PgPool;
 
 #[derive(Clone)]
 pub struct AppState {
-    pub db_pool: sqlx::PgPool,
+    pub db_pool: PgPool,
     pub settings: Settings,
 }
