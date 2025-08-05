5a8a65c26d7d6c65f12b74eb6f6226f2bb542491
Taiga Ishida <tonythetaiga@gmail.com>
2025-08-05 16:31:00 +0900

    use lucide icons

--- web/src/routes/dashboard/WorkspaceColumn.svelte
+++ web/src/routes/dashboard/WorkspaceColumn.svelte
@@ -6,6 +6,7 @@
   import { setSelectedWorkspace } from "./state.svelte";
   import WorkspaceList from "$lib/components/lists/WorkspaceList.svelte";
   import CreateWorkspaceModal from "$lib/components/modals/create-workspace-modal.svelte";
+  import { Mail, FolderPlus } from "@lucide/svelte";
 
   let { workspaces, workspaceRoles, workspaceInvitations } = $props();
   let workspaceSearchQuery = $state("");
@@ -25,50 +26,14 @@
         onclick={() => openCreateWorkspaceModal()}
         class="floating-element px-3 py-2 md:px-4 flex-shrink-0"
       >
-        <div class="flex items-center gap-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="lucide lucide-folder-plus-icon lucide-folder-plus"
-            ><path d="M12 10v6" /><path d="M9 13h6" /><path
-              d="M20 20a2 2 0 0 0 2-2V8a2 2 0 0 0-2-2h-7.9a2 2 0 0 1-1.69-.9L9.6 3.9A2 2 0 0 0 7.93 3H4a2 2 0 0 0-2 2v13a2 2 0 0 0 2 2Z"
-            /></svg
-          >
-        </div>
+        <FolderPlus />
       </button>
 
       <button
         aria-label="pending-invitations"
         class="floating-element px-3 py-2 md:px-4 flex-shrink-0"
       >
-        <div>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="lucide lucide-mail-icon lucide-mail"
-            ><path d="m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7" /><rect
-              x="2"
-              y="4"
-              width="20"
-              height="16"
-              rx="2"
-            /></svg
-          >
-        </div>
+        <Mail />
       </button>
     </div>
   </div>
