b00626f7ae74953bd53d61e97d3d7b47e36b9346
Taiga Ishida <tonythetaiga@gmail.com>
2025-09-10 14:54:16 +0900

    update api url

--- ios/tora/Services/Config.swift
+++ ios/tora/Services/Config.swift
@@ -5,6 +5,6 @@ import Foundation
 enum Config {
     // MARK: - Properties
 
-    static let baseURL = "https://tora-api-1030250455947.us-central1.run.app"
+    static let baseURL = "https://api.toracker.com"
     //    static let baseURL = "http://localhost:8080"
 }
